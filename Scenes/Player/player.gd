51bcfa9998d1881ed294446a1983dddc2d231930
felix <f.a.r.barrette@hotmail.com>
2024-10-26 19:13:05 -0400

    updated map, collision work for wall, player implemented

---
+++ Scenes/Player/player.gd
@@ -0,0 +1,11 @@
+extends CharacterBody2D
+
+
+const SPEED = 10000.0
+const JUMP_VELOCITY = -400.0
+
+
+func _physics_process(delta):
+	var direction = Input.get_vector("left", "right", "up", "down")
+	velocity = direction * SPEED * delta
+	move_and_slide()
