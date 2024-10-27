fd0bb48920bcb62c0d6da4ac18070eefd5a5abab
felix <f.a.r.barrette@hotmail.com>
2024-10-27 17:32:42 -0400

    los for zombies work properly, zombie also stop before coming to close to player

--- Scripts/CoreScripts/game.gd
+++ Scripts/CoreScripts/game.gd
@@ -53,14 +53,17 @@ func _on_retry_btn_pressed():
 func create_some_zombies():
 	var i = 0
 	var new_zomb
-	while (i < 2):
+	var begin
+	var end
+	while (i < 1):
 		new_zomb = zombie.instantiate()
-		#print(test_map.zombie_point_container.get_child(randi() % 20).global_position)
-		#print(new_zomb.default_route[0])
+		begin = test_map.zombie_point_container.get_child(randi() % 20).global_position
+		end = test_map.zombie_point_container.get_child(randi() % 20).global_position
+		while (end == begin):
+			end = test_map.zombie_point_container.get_child(randi() % 20).global_position
+		new_zomb.position = begin
 		zombie_container.add_child(new_zomb)
-		new_zomb.default_route[0] = to_local(test_map.zombie_point_container.get_child(randi() % 20).global_position)
-		new_zomb.default_route[1] = to_local(test_map.zombie_point_container.get_child(randi() % 20).global_position)
-		new_zomb.position = new_zomb.default_route[0]
-		print(new_zomb.default_route[0])
-		print(new_zomb.default_route[1])
+		new_zomb.default_route[0] = begin
+		new_zomb.default_route[1] = end
+		new_zomb.set_route()
 		i += 1
