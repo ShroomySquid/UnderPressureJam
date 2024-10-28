0de2196e0f8b26056a140b2797f757ad603636e2
felix <f.a.r.barrette@hotmail.com>
2024-10-28 16:04:38 -0400

    started first level, implemented player sprite, y sorted

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
 @onready var type = "player"
+@onready var animations = $IdleAnimation
 
 const SPEED = 10000.0
 const JUMP_VELOCITY = -400.0
@@ -9,3 +10,7 @@ func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
 	velocity = direction * SPEED * delta
 	move_and_slide()
+	run_animation()
+	
+func run_animation():
+	animations.play("Idle")
